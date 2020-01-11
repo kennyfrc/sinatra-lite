7d9d12cb77835f5c95ba133935c1d3b71425eb38
kennyfrc <fxkennyfrc@gmail.com>
2020-01-11 21:33:35 +0800

    advice giver

--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
+require 'pry-byebug'
+
 class SomeApp
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hey, the time right now is #{Time.now}"]]
+    case env['REQUEST_PATH']
+    when '/'
+      [200, {"Content-Type" => "text/plain"}, ["Hey, the time right now is #{Time.now}"]]
+    when '/advice'
+      [200, {"Content-Type" => "text/plain"}, ["Hey, the time right now is #{Advice.new.generate}"]]
+    else
+      [404, {"Content-Type" => "text/plain"}, ["Page does not exist."]]
+    end
+  end
+end
+
+# models
+
+class Advice
+  def initialize
+    @advice_list = [
+      "Look deep into nature, and then you will understand everything better.",
+      "I have found the paradox, that if you love until it hurts, there can be no more hurt, only more love.",
+      "What we think, we become.",
+      "Love all, trust a few, do wrong to none.",
+      "Oh, my friend, it's not what they take away from you that counts. It's what you do with what you have left.",
+      "Lost time is never found again.",
+      "Nothing will work unless you do."
+    ]
+  end
+
+  def generate
+    @advice_list.sample
   end
 end
